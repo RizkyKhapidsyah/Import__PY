96eaaa30972332b93543c373feda674305325241
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-01-02 10:07:36 +0700

    Add project files.

---
+++ Import__PY.py
@@ -0,0 +1,9 @@
+import Module_Bagian1
+import Module_Bagian2
+
+print(Module_Bagian1.Data)
+Module_Bagian1.cek_modul()
+Module_Bagian2.x
+print("")
+
+
---
+++ Module_Bagian1.py
@@ -0,0 +1,8 @@
+Data = "Ini adalah data yang di print dari module"
+
+def cek_modul():
+    print("Halloo.. Saya Rizky Khapidsyah")
+
+print("Ini Adalah Module")
+
+
